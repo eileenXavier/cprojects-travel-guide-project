b86a088d8107b7fafc7b6b7896840514a7510c05
eileenXavier <165471889+eileenXavier@users.noreply.github.com>
2024-11-18 22:36:10 +0700

    Update main.c

--- main.c
+++ main.c
@@ -9,280 +9,197 @@
 #define BCKSPC 8
 
 struct user{
-	char fullName[50];
-	char email[50];
-	char password[50];
-	char username[50];
-	char phone[50];
+    char fullName[50];
+    char email[50];
+    char password[50];
+    char username[50];
+    char phone[50];
 };
-    FILE *fp;
-	FILE *fpointer;
-    FILE *f;
-    char buffer2[200];
-    char place[15];
-    int found = 0;
-	int opt,usrFound = 0;
-	struct user user;
-	char password2[50],buffer[200];
-	 char pword1[64];
-	 char username1[64];
-   char buffer1[200];
-   int i = 0;
+FILE *fp;
+FILE *fpointer;
+FILE *f;
+char buffer2[200];
+char place[15];
+int found = 0;
+int opt, usrFound = 0;
+struct user user;
+char password2[50], buffer[200];
+char pword1[64];
+char username1[64];
+char buffer1[200];
+int i = 0;
 
 void takeinput(char ch[50]){
-	fgets(ch,50,stdin);
-	ch[strlen(ch) - 1] = 0; 
+    fgets(ch, 50, stdin);
+    ch[strlen(ch) - 1] = 0;
 }
 
-char generateUsername(char email[50],char username[50]){
-	//abc123@gmail.com
-	for(int i=0;i<strlen(email);i++){
-		if(email[i] == '@') break;
-		else username[i] = email[i];
-	}
+char generateUsername(char email[50], char username[50]){
+    for (int i = 0; i < strlen(email); i++) {
+        if (email[i] == '@') break;
+        else username[i] = email[i];
+    }
 }
 
 void takepassword(char pwd[50]){
-		int i;
-	char ch;
-	while(1){
-		ch = getch();
-		if(ch == ENTER || ch == TAB){
-			pwd[i] = '\0';
-			break;
-		}else if(ch == BCKSPC){
-			if(i>0){
-				i--;
-				printf("\b \b");
-			}
-		}else{
-			pwd[i++] = ch;
-			printf("* \b");
-		}
-	}
-				
-fpointer =fopen("record.txt", "r");
-   if (!fpointer) {
-      printf ("Results.dat could not be opened.\n");
-      exit(-1);
-   }
-
-   while( fgets (buffer, 200, fpointer)) {
-        if(strcmp(buffer,user.password) == 0) {
-          printf("\nplz create a strong password\n");
-          printf("Enter your password:\t");
-		 takepassword(user.password);
-       }
-   }
+    int i = 0;
+    char ch;
+    while (1) {
+        ch = getch();
+        if (ch == ENTER || ch == TAB) {
+            pwd[i] = '\0';
+            break;
+        } else if (ch == BCKSPC) {
+            if (i > 0) {
+                i--;
+                printf("\b \b");
+            }
+        } else {
+            pwd[i++] = ch;
+            printf("# \b"); // Changed mask character to '#'
+        }
+    }
 }
-
 
 int main(){
-	restart:
-	system("cls");
-	system("color 0b");
-	printf("\n\t\t\t\t----------Welcome to travel guide ----------");
-	printf("\nPlease choose your operation");
-	printf("\n1.Signup");
-	printf("\n2.Login");
-	printf("\n3.Exit");
-	
-	printf("\n\nYour choice:\t");
-	scanf("%d",&opt);
-	fgetc(stdin);
-	
-	switch(opt){
-		case 1:
-			system("cls");
-			printf("\nEnter your full name:\t");
-			takeinput(user.fullName);
-			printf("Enter your email:\t");
-			takeinput(user.email);
-			printf("Enter your contact no:\t");
-			takeinput(user.phone);
-			printf("Enter your password:\t");
-			takepassword(user.password);
+restart:
+    system("cls");
+    system("color 0b");
+    printf("\n\t\t\t\t----------Welcome to travel guide ----------");
+    printf("\nPlease choose your operation");
+    printf("\n1.Signup");
+    printf("\n2.Login");
+    printf("\n3.Exit");
+    printf("\n\nYour choice:\t");
+    scanf("%d", &opt);
+    fgetc(stdin);
 
-generateUsername(user.email,user.username);
-				
-fpointer = fopen("record.txt","a+");
-fprintf(fpointer,"%s",user.username);
-fprintf(fpointer,"\n");
-fprintf(fpointer,"%s",user.password);
-fprintf(fpointer,"\n");
-fclose(fpointer);
-      //embbed in file 
-       fp  = fopen("user.txt","a+");
-          fprintf(fp,"\n");
-          fprintf(fp,"%s				",user.fullName);
-          fprintf(fp,"%s				",user.email);
-         fprintf(fp,"%s				",user.phone);
-          fprintf(fp,"%s			",user.username);
-           fprintf(fp,"%s",user.password);
-               fclose(fp);
+    switch(opt){
+        case 1:
+            system("cls");
+            printf("\nEnter your full name:\t");
+            takeinput(user.fullName);
+            printf("Enter your email:\t");
+            takeinput(user.email);
+            printf("Enter your contact no:\t");
+            takeinput(user.phone);
+            printf("Enter your password:\t");
+            takepassword(user.password);
 
-				if(fprintf != 0) printf("\n\nUser resgistration success, Your username is %s",user.username);
-				else printf("\n\nSorry! Something went wrong :(");
-				fclose(fp);
-				system("cls");
-				printf("u can login now");
-				goto restart;
+            generateUsername(user.email, user.username);
 
-		break;
-		
-		case 2: //login	
-		login:
-  
-  
-  printf("Enter your username\t:");
- fgets(username1, 64, stdin);
+            fpointer = fopen("record.txt", "a+");
+            fprintf(fpointer, "%s", user.username);
+            fprintf(fpointer, "\n");
+            fprintf(fpointer, "%s", user.password);
+            fprintf(fpointer, "\n");
+            fclose(fpointer);
 
-   printf("enter the password:\t");
-fgets(pword1, 64, stdin);
+            fp = fopen("user.txt", "a+");
+            fprintf(fp, "\n");
+            fprintf(fp, "%s\t", user.fullName);
+            fprintf(fp, "%s\t", user.email);
+            fprintf(fp, "%s\t", user.phone);
+            fprintf(fp, "%s\t", user.username);
+            fprintf(fp, "%s", user.password);
+            fclose(fp);
 
+            if (fprintf != 0) printf("\n\nUser registration success, Your username is %s", user.username);
+            else printf("\n\nSorry! Something went wrong :(");
+            system("cls");
+            printf("You can login now");
+            goto restart;
 
-fpointer =fopen("record.txt", "a+");
-   if (!fpointer) {
-      printf ("Results.dat could not be opened.\n");
-      exit(-1);
-   }
+        break;
 
+        case 2:
+        login:
+            printf("Enter your username\t:");
+            fgets(username1, 64, stdin);
 
-   while( fgets(buffer, 200, fpointer)){
-        if(strcmp(buffer,username1) == 0){
-		  usrFound =1;
-       }
-   }
-   if (usrFound == 1){
-	   usrFound = 0;
-   }
-   else{
-	   printf("Incorrect username\n");
-	   goto login;
-   }
+            printf("Enter the password:\t");
+            fgets(pword1, 64, stdin);
 
-   fclose(fpointer);
+            fpointer = fopen("record.txt", "a+");
+            if (!fpointer) {
+                printf ("Results.dat could not be opened.\n");
+                exit(-1);
+            }
 
-   fpointer =fopen("record.txt", "a+");
+            while (fgets(buffer, 200, fpointer)) {
+                if (strcmp(buffer, username1) == 0) {
+                    usrFound = 1;
+                }
+            }
+            if (usrFound == 1) usrFound = 0;
+            else {
+                printf("Incorrect username\n");
+                goto login;
+            }
 
-   while( fgets(buffer, 200, fpointer)){
-        if(strcmp(buffer,pword1) == 0){
-          printf("you are registered");
-		  usrFound =1;
-       }
-   }
+            fclose(fpointer);
 
-   fclose(fpointer);
+            fpointer = fopen("record.txt", "a+");
 
-   if (usrFound == 1){
+            while (fgets(buffer, 200, fpointer)) {
+                if (strcmp(buffer, pword1) == 0) {
+                    printf("You are registered");
+                    usrFound = 1;
+                }
+            }
 
-   }
-   else{
-	   printf("Incorrect password\n");
-	   goto login;
-   }
+            fclose(fpointer);
 
-  goto guide;
-  
-break;
-		case 3:
-			printf("\t\t\tBye Bye :)");
-			return 0;		
-	}
+            if (usrFound == 1) {}
+            else {
+                printf("Incorrect password\n");
+                goto login;
+            }
 
-guide:
-     system("cls");
-    printf("Enter the place u want to search:");
-    fgets(place, 15, stdin);
+        goto guide;
 
+        break;
 
- for ( i = 0; i <= strlen (place); i++)  
- {  
- if (place[i] >= 65 && place[i] <= 90)  
- place[i] = place[i] + 32; /* add 32 to string character to convert into lower case. */  
- } 
-
-
-
-f =fopen("places.txt", "a+");
-   if (!f) {
-      printf ("Results.dat could not be opened.\n");
-      exit(-1);
-   }
-
-
-
-    while( fgets(buffer, 200, f)) {
-       
-        if(strcmp(buffer, place) == 0){
-          found = 1;
-		 
-          break;   
-   }
-   
+        case 3:
+            printf("\t\t\tBye Bye :)");
+            return 0;        
     }
 
-	if (found == 1) {}
-	else{ 
-	printf("Please mention the place name correctly");
-	Sleep(800);
-	fflush(stdin);
-	goto guide;
-}
+guide:
+    system("cls");
+    printf("Enter the place you want to search:");
+    fgets(place, 15, stdin);
 
-//getting the current position of the pointer
-fseek(f, 0, SEEK_CUR);
-    int pos=ftell(f);
+    for (i = 0; i <= strlen(place); i++) {
+        if (place[i] >= 65 && place[i] <= 90)
+            place[i] = place[i] + 32; 
+    }
 
-while (fscanf(f,"%s",buffer))
-{
-  if(strcmp(buffer,"}")==0){
-          break;
-   }
-}
-//getting the (reuired)end position of the pointer
-fseek(f, 0, SEEK_CUR);
-  int pos1=ftell(f);
+    f = fopen("places.txt", "a+");
+    if (!f) {
+        printf ("Results.dat could not be opened.\n");
+        exit(-1);
+    }
 
-//getting the reversed position of the posinter
-int exactpos = pos1 - pos;
-fseek(f, -exactpos, SEEK_CUR);
+    while (fgets(buffer, 200, f)) {
+        if (strcmp(buffer, place) == 0) {
+            found = 1;
+            break;   
+        }
+    }
 
- while( fgets(buffer,200, f) ) {
-        if(ftell(f)== pos1+2){
-			printf("\nsearch another place(1-- yes, 0--no)\n");
-          break;
-   }
-   else{
-      printf("\n%s",buffer);
-   }
-   
-  
- }
+    if (found == 1) {}
+    else {
+        printf("Please mention the place name correctly");
+        Sleep(800);
+        fflush(stdin);
+        goto guide;
+    }
 
+    while (fgets(buffer, 200, f)) {
+        printf("%s", buffer);
+    }
 
-if (found ==1){
-scanf("%d",&opt);
-resp:
-switch(opt){
-	case 1 : system("cls");
-	fflush(stdin);
-	 goto guide;
-	break;
-
-	case 0 : return 0;
-	break;
-
-	default : 
-	printf("\nsearch another place(1-- yes, 0--no)\n");
-scanf("%d",&opt);
-	goto resp;
-	break;
-}
-}
-
-
-
-  fclose(f);
-	return 0;
+    fclose(f);
+    return 0;
 }
